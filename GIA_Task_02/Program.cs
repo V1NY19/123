bf9f6b698b3a8344986710a6486e23d2b7c5afd6
V1NY <V1NY@DESKTOP-A2PDS9K>
2022-03-12 11:19:19 +0300

    Добавьте файлы проекта.

---
+++ GIA_Task_02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GIA_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите exit для остановки цикла");
+            string a = "exit";
+            string b = "";
+            while (b != a)
+
+            {
+                Console.Write("Цикл работает, для остановки напиши exit ");
+                b = Console.ReadLine();
+            }
+            Console.WriteLine("Цикл завершён");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TrainingPractice_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько у вас золота?");
+            int temp_gold;
+            temp_gold = Convert.ToInt32(Console.ReadLine());
+            int crystal_price = 3;
+            Console.WriteLine("Вы можете купить максимум " + temp_gold / crystal_price + " кристалов по цене " + crystal_price + " за штуку");
+            Console.WriteLine("Сколько хотите купить кристалов?");
+            int buy_crystal;
+            buy_crystal = Convert.ToInt32(Console.ReadLine());
+            int max_buy_crystal = temp_gold / crystal_price;
+            if (buy_crystal > max_buy_crystal)
+            {
+                Console.WriteLine("Вы не можете купить столько кристалов " + ", у вас всего лишь " + temp_gold + " золота. ");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Вы купили " + buy_crystal + " кристалов. У вас осталось " + (temp_gold - buy_crystal * crystal_price) + " золота");
+                Console.ReadKey();
+            }
+        }
+    }
+}
